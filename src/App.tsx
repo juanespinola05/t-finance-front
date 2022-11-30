1207b5c607a51e9a6b96dacd103504441aa30481
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-30 19:37:27 -0300

    render new components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { ReactElement } from 'react'
+import Navbar from './components/Navbar'
+import Routing from './Routing'
 
 function App (): ReactElement {
   return (
-    <div />
+    <div className='app'>
+      <main className='mobile-only template'>
+        <div>
+          <Routing />
+        </div>
+        <Navbar />
+      </main>
+    </div>
   )
 }
 
