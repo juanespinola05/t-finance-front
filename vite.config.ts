c0d6beadae0f2f15d38d3d82883fddc6b9105e61
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-30 19:35:35 -0300

    add roboto font

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginFonts } from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: [
+          {
+            /**
+             * Family name (required)
+             */
+            name: 'Roboto',
+
+            /**
+             * Family styles
+             */
+            styles: 'wght@300;400;700;900',
+
+            /**
+             * enable non-blocking renderer
+             *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
+             * default: true
+             */
+            defer: true,
+          },
+        ]
+      }
+    })
+  ]
 })
