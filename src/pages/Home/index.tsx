bf73e517a95e67c8ad32f55e6cb78a9b9e1fe09a
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-30 19:36:15 -0300

    create initial Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react'
+
+const Home = (): ReactElement => {
+  return (
+    <div className='font-roboto h-[18em] bg-blue text-center text-white grid place-content-center'>
+      <div className='flex flex-col'>
+        <p className='text-lg font-light'>Balance</p>
+        <p className='text-5xl font-bold mb-6'>$3,050</p>
+        <p className='font-light'> &lt; May 2022 &gt; </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
