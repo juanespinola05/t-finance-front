4b12fe7ca757d1160ad78a501ee00739a8da7ff4
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-11-30 08:47:03 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+import './App.css'
+
+function App (): ReactElement {
+  const x = 'hola'
+  console.log(x)
+  return (
+    <div className='App' />
+  )
+}
+
+export default App
