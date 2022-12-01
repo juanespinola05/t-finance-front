fc2fa338930359e14a9a22bf8861bdb4d4bcd0bf
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-01 09:41:00 -0300

    make navbar not render when not auth

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,21 @@
 import { ReactElement } from 'react'
+import useAuth from '../../hooks/useAuth'
 import { routes } from '../../Routing/routes'
 import NavItem from '../NavItem'
 
-const Navbar = (): ReactElement => {
+const Navbar = (): ReactElement | null => {
+  const { auth: { userInfo } } = useAuth()
+
+  if (userInfo === null) return null
+
   return (
     <nav className='bg-white drop-shadow-2xl'>
       <ul className='flex items-center justify-center gap-4 h-full'>
         {
-          routes.map(({ path, icon }) => (
-            <NavItem to={path} key={path}>
-              {icon()}
-            </NavItem>
+          routes.map(({ path, icon, render }) => (
+            render
+              ? <NavItem to={path} key={path}> {icon()} </NavItem>
+              : null
           ))
         }
       </ul>
