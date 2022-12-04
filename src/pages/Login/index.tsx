6a6ed279828ca8cd4194e71996ce4e7d49cdf30c
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-04 19:18:18 -0300

    update login view

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,40 @@
 import { ReactElement } from 'react'
-import { Navigate } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth'
+import AnimatedPage from '../../components/AnimatedPage'
+import Title from '../../components/Title'
+import Text, { TextSizes } from '../../components/Text'
+import LoginForm from '../../components/LoginForm'
 
 const Login = (): ReactElement => {
   const { auth: { userInfo } } = useAuth()
-
   if (userInfo !== null) return <Navigate to='/dashboard' />
 
   return (
-    <div>
-      <h1>Login here bro</h1>
-    </div>
+    <AnimatedPage>
+      <div className='p-4 text-center h-full'>
+        <div className='grid place-content-center h-full'>
+          <div>
+            <Title className='my-3'>
+              Welcome back
+            </Title>
+            <Text size={TextSizes.tiny} className='mb-8'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet deserunt pariatur nobis, at minus reprehenderit
+            </Text>
+          </div>
+          <div>
+            <LoginForm />
+            <div className='flex items-center justify-center gap-1 mt-10'>
+              <span className='block w-20 bg-gradient-to-l from-blue to-blue-500 h-1' />
+              <Link to='/register'>
+                Or Sign Up
+              </Link>
+              <span className='block w-20 bg-gradient-to-r from-blue to-blue-500 h-1' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </AnimatedPage>
   )
 }
 
