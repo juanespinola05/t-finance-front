b54e0350d024cbeada065f0448b6217ce9d74063
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-04 19:16:52 -0300

    setup form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,16 @@
+import { FC, HTMLAttributes, PropsWithChildren, ReactElement } from 'react'
+
+interface FormProps extends HTMLAttributes<HTMLFormElement> {}
+
+const Form: FC<PropsWithChildren<FormProps>> = ({ children, className = '', ...props }: PropsWithChildren<FormProps>): ReactElement => {
+  return (
+    <form
+      className={`flex flex-col gap-4 ${className}`}
+      {...props}
+    >
+      {children}
+    </form>
+  )
+}
+
+export default Form
