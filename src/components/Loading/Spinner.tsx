499177ac53499bb0a5d3bb1cd1f39b8d9d1ba22b
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 12:45:47 -0300

    change spinner styles

--- src/components/Loading/Spinner.tsx
+++ src/components/Loading/Spinner.tsx
@@ -2,7 +2,7 @@ import { FC, ReactElement } from 'react'
 
 const Spinner: FC = (): ReactElement => {
   return (
-    <span className='animate-spin border-4 border-blue border-t-transparent w-12 h-12 rounded-full left-0 right-0 m-auto top-0 bottom-0 absolute' />
+    <span className='animate-spin border-[5px] border-blue border-t-transparent w-12 h-12 rounded-full left-0 right-0 m-auto top-0 bottom-0 absolute z-50' />
   )
 }
 
