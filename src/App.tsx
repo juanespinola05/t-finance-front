956b6a85b81062a2413c138eb17551ab74f1f62b
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 11:38:21 -0300

    remove action and navbar from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
-import { ReactElement, useEffect } from 'react'
-import Loading from './components/Loading'
-import Navbar from './components/Navbar'
-import useAuth from './hooks/useAuth'
+import { ReactElement } from 'react'
 import Routing from './Routing'
 
 function App (): ReactElement {
-  const { getToken, auth: { loading } } = useAuth()
-
-  useEffect(() => {
-    getToken()
-  }, [])
-  if (loading) return <Loading />
-
   return (
     <div className='app'>
       <div className='wave' />
       <main className='mobile-only template'>
         <Routing />
-        <Navbar />
       </main>
     </div>
   )
