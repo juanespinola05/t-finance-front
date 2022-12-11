9543f229cb91d33f00400bd647dca4f59adafb52
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 11:39:36 -0300

    create error alert

---
+++ src/components/ErrorAlert/index.tsx
@@ -0,0 +1,24 @@
+import { FC, PropsWithChildren, ReactElement } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import Alert from '../Alert'
+import { BiErrorCircle } from 'react-icons/bi'
+
+interface ErrorAlertProps {
+  error: any
+}
+
+const ErrorAlert: FC<PropsWithChildren<ErrorAlertProps>> = ({ children, error }: PropsWithChildren<ErrorAlertProps>): ReactElement => {
+  return (
+    <AnimatePresence>
+      {
+        error !== null &&
+          <Alert>
+            <BiErrorCircle className='text-red-700 text-xl' />
+            {error}
+          </Alert>
+      }
+    </AnimatePresence>
+  )
+}
+
+export default ErrorAlert
