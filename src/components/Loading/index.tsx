57039d9339f35731e4380507949453f427949d4d
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 11:40:11 -0300

    make loading screen motion

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,15 +1,19 @@
 import { ReactElement } from 'react'
+import { motion } from 'framer-motion'
 import logo from '../../../public/logo.png'
 
 const Loading = (): ReactElement => {
   return (
-    <div className='bg-slate-100 h-full w-full absolute grid place-content-center'>
+    <motion.div
+      exit={{ opacity: 0 }}
+      className='bg-slate-100 h-[calc(100%-4rem)] z-50 w-full absolute grid place-content-center'
+    >
       <img
         src={logo}
         alt='T-Finance Logo'
         className='w-32 animate-pulse'
       />
-    </div>
+    </motion.div>
   )
 }
 
