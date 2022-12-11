2fe6b67465a0c724003c92fbc4227d91c7f6fe23
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 12:03:40 -0300

    update layout and move remove logic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,24 +1,20 @@
-import { ReactElement, useEffect } from 'react'
-import { getBalances } from '../../store/actions/balance.actions'
-import { useAppDispatch, useAppSelector } from '../../store/store'
+import { ReactElement } from 'react'
+import AnimatedPage from '../../components/AnimatedPage'
+import LoggedInLayout from '../../components/LoggedInLayout'
+import Navbar from '../../components/Navbar'
+
+import DashboardBanner from '../../DashboardBanner'
 
 const Dashboard = (): ReactElement => {
-  const { balance, totalIncome, totalOutflow } = useAppSelector(state => state.balance)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    console.log({ totalIncome, totalOutflow })
-    void dispatch(getBalances())
-  }, [])
-
   return (
-    <div className='font-roboto h-[18em] bg-blue text-center text-white grid place-content-center'>
-      <div className='flex flex-col'>
-        <p className='text-lg font-light'>Balance</p>
-        <p className='text-5xl font-bold mb-6'>${balance}</p>
-        <p className='font-light'> &lt; May 2022 &gt; </p>
-      </div>
-    </div>
+    <AnimatedPage>
+      <LoggedInLayout>
+        <div className='bg-lightBg'>
+          <DashboardBanner />
+        </div>
+        <Navbar />
+      </LoggedInLayout>
+    </AnimatedPage>
   )
 }
 
