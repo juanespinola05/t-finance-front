55a3d587f66699d95dcd64667a316dddd81df153
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 11:49:01 -0300

    add missing method in useAuth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,14 +1,21 @@
+import { loginUser } from '../store/actions/auth.actions'
 import { AuthState, setToken } from '../store/slices/auth.slice'
 import { useAppDispatch, useAppSelector } from '../store/store'
+import { LoginInput } from '../types/auth'
 
 interface useAuthInterface {
   auth: AuthState
   getToken: Function
+  login: Function
 }
 
 const useAuth = (): useAuthInterface => {
   const dispatch = useAppDispatch()
   const auth = useAppSelector((state) => state.auth)
+
+  const login = (loginData: LoginInput): void => {
+    void dispatch(loginUser(loginData))
+  }
 
   const getToken = (): void => {
     const token = localStorage.getItem('token')
@@ -17,7 +24,8 @@ const useAuth = (): useAuthInterface => {
 
   return {
     auth,
-    getToken
+    getToken,
+    login
   }
 }
 
