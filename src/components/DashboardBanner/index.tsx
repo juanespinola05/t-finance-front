b1f71e53bb09b33105f1dc3c080a46a0ad80cdfc
Juan Sebastian Espinola <sebar5er@gmail.com>
2022-12-11 12:45:57 -0300

    add loading to dashboard

--- src/components/DashboardBanner/index.tsx
+++ src/components/DashboardBanner/index.tsx
@@ -2,24 +2,32 @@ import { FC, ReactElement } from 'react'
 import MonthSwitcher from '../MonthSwitcher'
 import TotalAmount from '../TotalAmount'
 import { useAppSelector } from '../../store/store'
+import Spinner from '../Loading/Spinner'
 
 const DashboardBanner: FC = (): ReactElement => {
-  const { balance, totalIncome, totalOutflow } = useAppSelector(state => state.balance)
+  const { balance, totalIncome, totalOutflow, loading } = useAppSelector(state => state.balance)
 
   return (
-    <div>
-      <div className='font-roboto h-[18em] bg-blue text-center text-white grid place-content-center'>
-        <div className='flex flex-col'>
-          <p className='text-lg font-light'>Balance</p>
-          <p className='text-5xl font-bold mb-6'>${balance}</p>
-          <MonthSwitcher />
+    <>
+      {
+        loading && (
+          <Spinner />
+        )
+      }
+      <div className={loading ? 'blur-[1px]' : ''}>
+        <div className='font-roboto h-[18em] bg-blue text-center text-white grid place-content-center'>
+          <div className='flex flex-col'>
+            <p className='text-lg font-light'>Balance</p>
+            <p className='text-5xl font-bold mb-6'>${balance}</p>
+            <MonthSwitcher />
+          </div>
+        </div>
+        <div className='flex justify-around relative h-20 rounded-2xl w-[90%] m-auto top-[-40px] bg-white'>
+          <TotalAmount amount={totalIncome} type='income' />
+          <TotalAmount amount={totalOutflow} type='outflow' />
         </div>
       </div>
-      <div className='flex justify-around relative h-20 rounded-2xl w-[90%] m-auto top-[-40px] bg-white'>
-        <TotalAmount amount={totalIncome} type='income' />
-        <TotalAmount amount={totalOutflow} type='outflow' />
-      </div>
-    </div>
+    </>
   )
 }
 
