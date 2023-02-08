34aa41bf449310ab69b29d10a52c959ad6e80f31
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-08 11:46:09 -0300

    change banner to gradient

--- src/pages/Setings/index.tsx
+++ src/pages/Setings/index.tsx
@@ -7,7 +7,7 @@ const Settings: FC = (): ReactElement => {
     <AnimatedPage>
       <LoggedInLayout>
         <div className='bg-white'>
-          <div className='font-roboto h-[18em] bg-yellow-300 text-center text-black grid place-content-center'>
+          <div className='font-roboto h-[18em] bg-gradient-to-r from-[#456fe8] to-[#19b0ec] text-center text-white grid place-content-center'>
             <div className='flex flex-col'>
               <img
                 className='rounded-full p-1 border-2 border-white border-solid'
@@ -15,7 +15,7 @@ const Settings: FC = (): ReactElement => {
                 alt='Random Avatar Image'
               />
               <p className='text-xl font-bold'>Juan Sebas</p>
-              <p className='text-md text-gray-900'>sebar5er@gmail.com</p>
+              <p className='text-md font-light'>sebar5er@gmail.com</p>
               <a href='/logout' className='underline'>Log out</a>
             </div>
           </div>
